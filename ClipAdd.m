57dd010a86971e431b4ccff4975f63b2e0882e48
Zebedeus <suodatin_pussi@hotmail.com>
2013-06-04 23:01:38 +0300

    Update ClipAdd.m

--- ClipAdd.m
+++ ClipAdd.m
@@ -1,6 +1,8 @@
 function [level,team]=ClipAdd(level,team,x,y,z,v,t,L)
 
-% add an object to the level
+% add an object to the level and remove out of scope data
+% other function could be more logical since this one overrides
+% previous objects
 
 for j=1:L
     if (x(j)>1) & (x(j)<64) & (y(j)>1) & (y(j)<64) & (z(j)>1) & (z(j)<64)
